a1c164ca3f438fd65aa1a70527c7de0c385cb072
cherylperyl <cheryl.goh.2020@smu.edu.sg>
2021-09-22 08:21:16 +0800

    Adds question files

---
+++ q1_employee/1b_code.py
@@ -0,0 +1,6 @@
+# Task: What is the average salary of employees who do not manage anyone? 
+# In the sample above, that would be John, Mike, Joe and Dan, since they do not have anyone reporting to them
+
+# 1 - find set of manger ids
+
+# 2 - for each employee
